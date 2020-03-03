a0f364edcae1c40db333949010d4ec0ee5c4e275
dream-ellie <dream.coder.ellie@gmail.com>
2020-03-03 22:37:28 +1100

    add initial project

---
+++ README.md
@@ -0,0 +1,8 @@
+# Responsive Nav Bar UI Component
+Responsive Nav Bar UI Component
+
+## Wide Screen
+![wide](https://github.com/dream-ellie/responsive-nav-bar/blob/master/demo/wide.png)
+
+## Small Screen
+![small](https://github.com/dream-ellie/responsive-nav-bar/blob/master/demo/small.png)
---
+++ main.js
@@ -0,0 +1,8 @@
+const toggleBtn = document.querySelector('.navbar__toggleBtn');
+const menu = document.querySelector('.navbar__menu');
+const icons = document.querySelector('.navbar__icons');
+
+toggleBtn.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  icons.classList.toggle('active');
+});
